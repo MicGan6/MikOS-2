518cd508d6c9d5264d0d9b2c131c27775d5695dd
GanGan1 <Tonyyyds666666@outlook.com>
2024-04-30 22:01:16 +0800

    Update MikDOS
    更新MikDOS

--- MikDOS/DOS/lib/DOSLib.py
+++ MikDOS/DOS/lib/DOSLib.py
@@ -1,11 +1,22 @@
 """
 本程序为MikDOS的运行库
+变量path为工作目录，p_path为上级目录
 """
-import os, sys
+import os, sys, time
+#帮助指令
 def helpdos(path):
     with open(path + '\\DOS\\Command.txt', mode='r', encoding='utf-8') as cmd:
         file = cmd.read()
         print(file)
+#启动MikOS
 def startmik(p_path):
     os.startfile(p_path + '\\logon.pyw')
-    
+#打开设置
+def setting(p_path):
+    os.startfile(p_path + '\\setting\\setting.pyw')
+#打印时间
+def p_time():
+    print(time.ctime())
+#延时打印(故意滴doge)
+def show_later():
+    time.sleep(1)
--- MikDOS/MikDOS_Main.py
+++ MikDOS/MikDOS_Main.py
@@ -1,6 +1,6 @@
 import os, sys
 import DOS.lib.DOSLib as DOSLib
-
+from DOS.lib.DOSLib import show_later
 # 获取工作目录和上级目录
 path = os.getcwd() + '\\MikDOS'
 print(path)
@@ -16,13 +16,22 @@ while True:
     comd = inp.lower()
     #判断
     if comd == 'help':
+        show_later()
         DOSLib.helpdos(path)
     elif comd == 'shutdown':
         print('正在关闭')
+        show_later()
         sys.exit()
     elif comd == 'startmik':
+        show_later()
         DOSLib.startmik(parent_path)
         sys.exit()
+    elif comd == 'setting':
+        show_later()
+        DOSLib.setting(parent_path)
+    elif comd == 'time':
+        show_later()
+        DOSLib.p_time()
     else:
         print('未知指令')
         continue
