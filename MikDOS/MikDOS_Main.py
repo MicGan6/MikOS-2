0c263cf9d1e7aad4356f1b71194dae262bcc19cd
GanGan1 <Tonyyyds666666@outlook.com>
2024-05-01 15:45:53 +0800

    Add More annotations
    增加注释

--- MikDOS/MikDOS_Main.py
+++ MikDOS/MikDOS_Main.py
@@ -15,23 +15,29 @@ while True:
     #统一输入格式
     comd = inp.lower()
     #判断
+    #帮助
     if comd == 'help':
         show_later()
         DOSLib.helpdos(path)
+    #关机
     elif comd == 'shutdown':
         print('正在关闭')
         show_later()
         sys.exit()
+    #启动MikOS
     elif comd == 'startmik':
         show_later()
         DOSLib.startmik(parent_path)
         sys.exit()
+    #打开设置
     elif comd == 'setting':
         show_later()
         DOSLib.setting(parent_path)
+    #打印时间
     elif comd == 'time':
         show_later()
         DOSLib.p_time()
+    #未输入指令
     else:
-        print('未知指令')
+        print(inp, '不是一个可以执行的命令')
         continue
